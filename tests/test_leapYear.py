fb6713ddf993f3157a26f22f8d1dd54a8c23d505
Bragvin <131401239+bragvin@users.noreply.github.com>
2023-10-24 22:26:56 +0200

    laget feilende test

--- tests/test_leapYear.py
+++ tests/test_leapYear.py
@@ -9,7 +9,7 @@ def test_leap_year_divisible_by_4_not_divisable_by_100():
     assert isLeapYear(2012) == True
 
 def test_year_is_not_leap_year(): #Feilende test
-    assert isLeapYear(2011) == False
+    assert isLeapYear(2011) == True
 
 
 def test_leap_year_divisible_by_400():
