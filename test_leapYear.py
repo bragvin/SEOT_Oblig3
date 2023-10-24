b1e7e9436fff5273d2bec6fc7900d9884cfa73ba
Bragvin <131401239+bragvin@users.noreply.github.com>
2023-10-24 22:01:09 +0200

    feat: added workflow for tests

--- test_leapYear.py
+++ test_leapYear.py
@@ -4,7 +4,7 @@ import pytest
 from leapYear import isLeapYear
 
 
-def test_leap_year_divisible_by_4_not_dividable_by_100():
+def test_leap_year_divisible_by_4_not_divisable_by_100():
     assert not isLeapYear(2003) == True
     assert isLeapYear(2011) == False
     assert isLeapYear(2012) == True
