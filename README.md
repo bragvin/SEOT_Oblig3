25110ec4a73b107c2830904ab2dda502d7aacd53
Bragvin <131401239+bragvin@users.noreply.github.com>
2023-10-24 19:30:36 +0200

    updates

---
+++ README.md
@@ -0,0 +1,8 @@
+# SEOT_Oblig2
+
+Oblig 2 for emnet Software Engineering og testing, ITF20319.
+
+Start dato 4. okt. 2023
+
+
+Frist 6. okt. 2023
---
+++ test_leapYear.py
@@ -0,0 +1,25 @@
+# test_leapYear.py
+
+import pytest
+from leapYear import isLeapYear
+
+
+def test_leap_year_divisible_by_4_not_divisible_by_100():
+    assert not isLeapYear(2003) == True
+    assert isLeapYear(2011) == False
+    assert isLeapYear(2012) == True
+
+
+def test_leap_year_divisible_by_400():
+    assert not isLeapYear(1998) == True
+    assert isLeapYear(2000) == True
+
+
+def test_non_leap_year_not_divisible_by_4():
+    assert isLeapYear(2003) == False
+    assert isLeapYear(2021) == False
+
+
+def test_non_leap_year_divisible_by_100_not_divisible_by_400():
+    assert isLeapYear(1800) == False
+    assert isLeapYear(1900) == False
